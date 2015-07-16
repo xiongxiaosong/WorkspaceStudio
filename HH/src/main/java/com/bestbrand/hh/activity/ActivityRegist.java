942d007fc609d7d60636111f8177764740fe797b
xiongxs <583155684@163.com>
2015-07-16 10:40:57 +0800

    完成登录验证码获取倒计时功能

--- HH/src/main/java/com/bestbrand/hh/activity/ActivityRegist.java
+++ HH/src/main/java/com/bestbrand/hh/activity/ActivityRegist.java
@@ -7,11 +7,16 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.bestbrand.hh.R;
 import com.bestbrand.hh.http.HttpClintClass;
 import com.bestbrand.hh.view.TitleBar;
 import com.xxs.sdk.manage.XActivityManager;
+import com.xxs.sdk.myinterface.MyTimerInterCallback;
+import com.xxs.sdk.util.MyTimerUtil;
 import com.xxs.sdk.util.ProveUtil;
 import com.xxs.sdk.util.UiUtil;
 
@@ -21,7 +26,7 @@ import com.xxs.sdk.util.UiUtil;
  * @author xiongxs
  * @date 2015-05-10
  */
-public class ActivityRegist extends ActivityBase {
+public class ActivityRegist extends ActivityBase implements MyTimerInterCallback {
     private static final String LOG_TAG = ActivityRegist.class.getName();
     private static final String ID_REGISTER = LOG_TAG + "register";
     /**
@@ -48,6 +53,22 @@ public class ActivityRegist extends ActivityBase {
      * 标题栏控件
      */
     private TitleBar mTitleBar;
+    /**
+     * 获取验证码的按钮
+     */
+    private FrameLayout getprovenum;
+    /**
+     * 倒计时进度条
+     */
+    private ProgressBar progressBar;
+    /**
+     * 获取验证码时的提示文字
+     */
+    private TextView progressmessage;
+    /**
+     * 计时器
+     */
+    private MyTimerUtil myTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +76,7 @@ public class ActivityRegist extends ActivityBase {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         findViewMethod();// 调用实例化控件的方法
+        initMethod();//调用初始化的方法
     }
 
     /**
@@ -68,9 +90,20 @@ public class ActivityRegist extends ActivityBase {
         editPasswordEnsure = (EditText) findViewById(R.id.activity_register_password_ensure);
         editProvenumber = (EditText) findViewById(R.id.activity_register_provenumber);
         buttonRegister = (Button) findViewById(R.id.activity_register_getnow);
+        getprovenum = (FrameLayout) findViewById(R.id.activity_register_getprove);
+        progressBar = (ProgressBar) findViewById(R.id.activity_register_progress);
+        progressmessage = (TextView) findViewById(R.id.activity_register_progressmessage);
         // 绑定监听事件
         buttonRegister.setOnClickListener(this);
         mTitleBar.setleftClickListener(this);
+        getprovenum.setOnClickListener(this);
+    }
+
+    /**
+     * 进行一些初始化的方法
+     */
+    private void initMethod() {
+        myTimer = new MyTimerUtil(60 * 1000, 1000, this);
     }
 
     @Override
@@ -82,11 +115,15 @@ public class ActivityRegist extends ActivityBase {
                 turnToLogin();//调用跳转到登录页面的方法
                 break;
             case R.id.activity_register_getnow:// 注册
-                if(proveInputTypeMethod()){
+                if (proveInputTypeMethod()) {
                     registerMethod();// 调用注册方法
                 }
                 break;
-
+            case R.id.activity_register_getprove://获取验证码
+                if (!myTimer.isstarted()) {
+                    myTimer.onStart();
+                }
+                break;
             default:
                 break;
         }
@@ -97,23 +134,23 @@ public class ActivityRegist extends ActivityBase {
      */
     private boolean proveInputTypeMethod() {
         if (TextUtils.isEmpty(editTelephone.getText().toString().trim())) {
-            UiUtil.setErrorMethod(editTelephone,getString(R.string.please_input_telephone));
+            UiUtil.setErrorMethod(editTelephone, getString(R.string.please_input_telephone));
             return false;
         }
         if (TextUtils.isEmpty(editPassword.getText().toString().trim())) {
-            UiUtil.setErrorMethod(editPassword,getString(R.string.please_input_password));
+            UiUtil.setErrorMethod(editPassword, getString(R.string.please_input_password));
             return false;
         }
         if (TextUtils.isEmpty(editPasswordEnsure.getText().toString().trim())) {
-            UiUtil.setErrorMethod(editPasswordEnsure,getString(R.string.please_input_password_ensure));
+            UiUtil.setErrorMethod(editPasswordEnsure, getString(R.string.please_input_password_ensure));
             return false;
         }
         if (TextUtils.isEmpty(editProvenumber.getText().toString().trim())) {
-            UiUtil.setErrorMethod(editProvenumber,getString(R.string.please_input_provenumber));
+            UiUtil.setErrorMethod(editProvenumber, getString(R.string.please_input_provenumber));
             return false;
         }
-        if(!ProveUtil.isMobile(editTelephone.getText().toString().trim())){
-            UiUtil.setErrorMethod(editProvenumber,getString(R.string.error_telephone_type));
+        if (!ProveUtil.isMobile(editTelephone.getText().toString().trim())) {
+            UiUtil.setErrorMethod(editProvenumber, getString(R.string.error_telephone_type));
             return false;
         }
         return true;
@@ -178,5 +215,18 @@ public class ActivityRegist extends ActivityBase {
     public void failExecuteHttp(String threadId, Exception e) {
         // TODO Auto-generated method stub
         super.failExecuteHttp(threadId, e);
+    }
+
+    @Override
+    public void timerLeft(long lefttime) {
+        progressmessage.setText(lefttime + "s");
+        progressBar.setProgress((int) (lefttime*100/60));
+    }
+
+    @Override
+    public void timerFinish() {
+        myTimer.cancel();
+        progressmessage.setText(getString(R.string.string_register_provenumber_getagain));
+        progressBar.setProgress(0);
     }
 }
--- XIONGSDK/src/main/java/com/xxs/sdk/util/MyTimerUtil.java
+++ XIONGSDK/src/main/java/com/xxs/sdk/util/MyTimerUtil.java
@@ -5,51 +5,82 @@ import com.xxs.sdk.myinterface.MyTimerInterCallback;
 
 /**
  * 自定义计时器
- * 
+ *
  * @author 熊小松
  * @time 2014-01-07
- * 
  */
 public class MyTimerUtil extends CountDownTimer {
-	/** 计时器回调接口 */
-	private MyTimerInterCallback callback;
+    /**
+     * 计时器回调接口
+     */
+    private MyTimerInterCallback callback;
 
-	/**
-	 * 构造函数
-	 * 
-	 * @param context
-	 *            上下文
-	 * @param millisInFuture
-	 *            倒计时间总数（单位毫秒）
-	 * @param countDownInterval
-	 *            每次计时间隔时间（单位毫秒）
-	 * @param myInterFace
-	 *            自定义计时器回调接口
-	 */
-	public MyTimerUtil(long millisInFuture, long countDownInterval,
-			MyTimerInterCallback callback) {
-		super(millisInFuture, countDownInterval);
-		// TODO Auto-generated constructor stub
-		this.callback = callback;
-	}
+    /**
+     * 是否已经启动倒计时
+     */
+    private boolean isstarted;
 
-	/**
-	 * 每次计时都会调用的方法
-	 */
-	@Override
-	public void onTick(long millisUntilFinished) {
-		// TODO Auto-generated method stub
-		callback.timerLeft(millisUntilFinished/1000);
-	}
+    /**
+     * 构造函数
+     *
+     * @param millisInFuture    倒计时间总数（单位毫秒）
+     * @param countDownInterval 每次计时间隔时间（单位毫秒）
+     * @param callback          自定义计时器回调接口
+     */
+    public MyTimerUtil(long millisInFuture, long countDownInterval,
+                       MyTimerInterCallback callback) {
+        super(millisInFuture, countDownInterval);
+        // TODO Auto-generated constructor stub
+        this.callback = callback;
+    }
 
-	/**
-	 * 计时完成后将要自动调用的方法
-	 */
-	@Override
-	public void onFinish() {
-		callback.timerFinish();
-	}
-	public void onCancle(){
-		callback.timerFinish();
-	}
+    /**
+     * 每次计时都会调用的方法
+     */
+    @Override
+    public void onTick(long millisUntilFinished) {
+        // TODO Auto-generated method stub
+        callback.timerLeft(millisUntilFinished / 1000);
+    }
+
+    /**
+     * 计时完成后将要自动调用的方法
+     */
+    @Override
+    public void onFinish() {
+        isstarted = false;
+        cancel();
+        callback.timerFinish();
+    }
+
+    /**
+     * 取消倒计时的方法
+     */
+    public void onCancle() {
+        isstarted = false;
+        cancel();
+    }
+
+    /**
+     * 启动倒计时的方法
+     */
+    public void onStart() {
+        isstarted = true;
+        start();
+    }
+
+    /**
+     * 获取倒计时是否已经启动
+     */
+    public boolean isstarted() {
+        return isstarted;
+    }
+
+    /**
+     * 设置倒计时是否已经启动
+     */
+    public void setIsstarted(boolean isstarted) {
+        this.isstarted = isstarted;
+    }
+
 }
